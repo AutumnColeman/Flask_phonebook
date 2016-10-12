8690082ff4d0bac168b35c3e005ec157b72a3458
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-10-12 15:45:53 -0400

    Added form

--- flask_phonebook.py
+++ flask_phonebook.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import pg
 
 db = pg.DB(dbname='phonebook_v2')
@@ -12,6 +12,32 @@ def list_all():
         'layout.html',
         title='Phonebook',
         entry_list=query.namedresult())
+    # redirect('/new_entry')
+
+@app.route('/new_entry')
+def new_entry():
+
+    return render_template(
+        'new_entry.html',
+        title='New Entry'
+    )
+
+@app.route('/submit_new_entry', methods=['POST'])
+def submit_form():
+    name = request.form.get('name')
+    cell_phone = request.form.get('cell_phone')
+    home_phone = request.form.get('home_phone')
+    work_phone = request.form.get('work_phone')
+    email = request.form.get('email')
+    db.insert(
+        'phonebook',
+        name=name,
+        cell_phone=cell_phone,
+        home_phone=home_phone,
+        work_phone=work_phone,
+        email=email)
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
